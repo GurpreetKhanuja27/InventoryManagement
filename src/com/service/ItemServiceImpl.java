6cd3ced411fe5da8ff48ff6e2c39636fec72ff77
Gurpreet <gurpreet.khanuja01@gmail.com>
2018-07-03 16:03:34 -0400

    Added service layer

---
+++ src/com/service/ItemServiceImpl.java
@@ -0,0 +1,21 @@
+package com.service;
+
+import java.util.List;
+
+import com.Item;
+
+public class ItemServiceImpl implements ItemService {
+
+	@Override
+	public void addItem(Item item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Item> getAllItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

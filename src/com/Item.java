b23d260fc61e2b9492fda292002870784ba36b3a
Gurpreet <gurpreet.khanuja01@gmail.com>
2018-07-03 15:46:00 -0400

    Included getters and setters, also constructor

--- src/com/Item.java
+++ src/com/Item.java
@@ -4,4 +4,26 @@ public class Item {
 
 	private String name;
 	private int id;
+	
+	
+	public Item(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
 }

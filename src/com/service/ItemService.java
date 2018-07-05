15d71f35d7a7c19bdb76f11c7cbf8388cdf45ebd
GurpreetKhanuja27 <38744459+GurpreetKhanuja27@users.noreply.github.com>
2018-07-05 12:15:27 -0400

    Switched from List to Set

--- src/com/service/ItemService.java
+++ src/com/service/ItemService.java
@@ -1,12 +1,12 @@
 package com.service;
 
-import java.util.List;
+import java.util.Set;
 
 import com.Item;
 
 public interface ItemService {
 
 	public void addItem(Item item);
-	public List<Item> getAllItems();
+	public Set<Item> getAllItems();
 	
 }
